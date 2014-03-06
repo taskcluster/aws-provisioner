5878d8be6f6b4d3b275db3369c97b36ee61dbcba
James Lal <james@lightsofapollo.com>
2014-03-05 21:48:27 -0800

    swap ou m1.small for c3.large
    m1.small is too slow until we can optimize away some aspects of how the gaia tests are setup.

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ var DEFAULT_CONFIG_VALUES = {
     'max-retries':                  5,
 
     // Instance type to launch
-    'instance-type':                'm1.small',
+    'instance-type':                'c3.large,
 
     // IAM profile assigned to instances launched
     'iam-profile':                  'taskcluster-worker',
