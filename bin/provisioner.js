291855fee667e4834df7ee80891613a3d9ad90eb
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-04-27 13:21:42 -0700

    Set default region for AWS, but omit it for Aws thingiy config

--- bin/provisioner.js
+++ bin/provisioner.js
@@ -9,7 +9,7 @@ var AwsManager = require('../provisioner/aws-manager');
 var awsPricing = require('../provisioner/aws-pricing');
 var Cache = require('../lib/cache');
 var taskcluster = require('taskcluster-client');
-
+var _ = require('lodash');
 
 
 var launch = function(profile) {
@@ -57,7 +57,7 @@ var launch = function(profile) {
 
   // Create all the things which need to be injected into the
   // provisioner
-  var ec2 = new Aws('EC2', cfg.get('aws'), allowedRegions);
+  var ec2 = new Aws('EC2', _.omit(cfg.get('aws'), allowedRegions);
   var awsManager = new AwsManager(ec2, keyPrefix, pubKey);
   var queue = new taskcluster.Queue({credentials: cfg.get('taskcluster:credentials')});
   var pricingCache = new Cache(15, awsPricing, ec2);
