e368dc3de82f3663f20495a34f906a7dcb00b49c
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-04-27 13:33:11 -0700

    minor fix ups...

--- bin/provisioner.js
+++ bin/provisioner.js
@@ -56,7 +56,7 @@ var launch = function(profile) {
 
   // Create all the things which need to be injected into the
   // provisioner
-  var ec2 = new Aws('EC2', _.omit(cfg.get('aws'), allowedRegions);
+  var ec2 = new Aws('EC2', _.omit(cfg.get('aws'), 'region'), allowedRegions);
   var awsManager = new AwsManager(ec2, keyPrefix, pubKey);
   var queue = new taskcluster.Queue({credentials: cfg.get('taskcluster:credentials')});
   var pricingCache = new Cache(15, awsPricing, ec2);
