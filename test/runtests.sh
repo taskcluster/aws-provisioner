ebe9fd7b11c63f39469e6f046b77f1fc7fa11f7e
John Ford <john@johnford.org>
2015-06-11 08:52:54 -0400

    Allow skipping linting with npm test

--- test/runtests.sh
+++ test/runtests.sh
@@ -2,14 +2,16 @@
 # USAGE: Run this file using `npm test` (must run from repository root)
 
 # Run linter
-eslint \
-  bin/server.js \
-  bin/provisioner.js \
-  bin/manage.js \
-  lib/*.js \
-  routes/*.js \
-  test/*.js \
-  provisioner/*.js
+if [ "$NOLINT" != 1 ] ; then
+  eslint \
+    bin/server.js \
+    bin/provisioner.js \
+    bin/manage.js \
+    lib/*.js \
+    routes/*.js \
+    test/*.js \
+    provisioner/*.js
+fi
 
 # Run tests
 mocha                                   \
