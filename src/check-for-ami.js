041cf53ce85a6b9e928a54ae800672c9c936b698
John Ford <john@johnford.org>
2016-04-15 14:56:23 +0200

    remove multi-region stuff and add tests for check-for-ami.js

--- src/check-for-ami.js
+++ src/check-for-ami.js
@@ -7,7 +7,7 @@ let assert = require('assert');
 
 let debug = require('debug')('aws-provisioner:lib:check-for-ami');
 
-module.exports = async function (ec2, ami, region) {
+module.exports = async function (ec2, ami) {
   let request = {
     /* Not sure why this isn't working
     ExecutableUsers: [
@@ -19,25 +19,18 @@ module.exports = async function (ec2, ami, region) {
 
   let result;
   try {
-    // If we're using the multi-region wrapper, we handle things
-    // slightly differently
-    if (ec2.describeImages && ec2.describeImages.inRegion) {
-      result = await ec2.describeImages.inRegion(region, request);
-    } else {
-      assert(!region, 'this ec2 module doesnt understand region');
-      result = await ec2.describeImages(request);
-    }
+    result = await ec2.describeImages(request).promise();
     debug('result from ec2: %j', result);
   } catch (err) {
     debug('did not find %s', ami);
     return false;
   }
 
-  if (result.Images.length !== 1) {
+  if (result.data.Images.length !== 1) {
     throw new Error('found more images than expected');
   }
 
-  if (result.Images[0].ImageId === ami) {
+  if (result.data.Images[0].ImageId === ami) {
     return true;
   } else {
     throw new Error('api returned incorrect ami for search parameters');
---
+++ test-src/checkForAmi_test.js
@@ -0,0 +1,35 @@
+let subject = require('../lib/check-for-ami');
+let assume = require('assume');
+var libConfig = require('taskcluster-lib-config');
+let aws = require('aws-sdk-promise');
+
+describe('ami check', () => {
+  let ec2;
+
+  before(() => {
+    let cfg = libConfig({
+      defaults: require('../config/defaults'),
+      profile: require('../config/test'),
+      filename: 'taskcluster-aws-provisioner',
+    });
+    let ec2conf = cfg.get('aws');
+    ec2conf.region = 'us-west-2';
+    ec2 = new aws.EC2(cfg.get('aws'));
+  });
+
+  it('should see that an existing ami is really there', async () => {
+    // NOTE: When amazon eventually deletes this AMI, we'll need
+    // to pick a new one.  I picked one from here:
+    // https://aws.amazon.com/amazon-linux-ami/
+    // They still have some from 2011 there... we're fine
+    let actual = await subject(ec2, 'ami-c229c0a2');
+    assume(actual).is.true();
+  });
+
+  it('should see that an absent ami is not there', async () => {
+    // NOTE: It's theoretically possible I guess for this to be
+    // an AMI Id, but highly unlikely
+    let actual = await subject(ec2, 'ami-00000000');
+    assume(actual).is.false();
+  });
+});
