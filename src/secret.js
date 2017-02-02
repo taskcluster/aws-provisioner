a7051e6beef25fd9d283c8434cd71eb4186ecaa5
Dustin J. Mitchell <dustin@mozilla.com>
2017-02-02 19:33:49 +0000

    Bug 1335507: sign and encrypt worker types and secrets
    With this change, someone intercepting a copy of our table backups will
    not get any secrets.  A restore will also verify the signature on the
    rows, avoiding the risk of modification.  Otherwise, there is no impact.

--- src/secret.js
+++ src/secret.js
@@ -15,6 +15,20 @@ let Secret = base.Entity.configure({
   },
 });
 
+// Encrypt the secrets column
+Secret = Secret.configure({
+  version: 2,
+  signEntities: true, // NEW
+  properties: {
+    token: base.Entity.types.String,
+    expiration: base.Entity.types.Date,
+    workerType: base.Entity.types.String,
+    secrets: base.Entity.types.EncryptedJSON,
+    scopes: base.Entity.types.JSON,
+  },
+  migrate: item => item, // no change in the column content, just format
+});
+
 Secret.prototype.modify = function() {
   throw new Error('No modifications of secrets are allowed');
 };
