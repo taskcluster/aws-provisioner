81bbe076754a2d5d872c84bba68d6198d068b84a
John Ford <john@johnford.org>
2017-03-07 20:29:10 +0100

    This is a version of e866a5aca608f8216cf487e82396e7e351eb60fd created by 'git show $cset | patch -p1'.  fixes #131

--- src/api-v1.js
+++ src/api-v1.js
@@ -372,7 +372,7 @@ api.declare({
   title: 'Get Worker Type',
   stability:  API.stability.stable,
   description: [
-    'Retreive a copy of the requested worker type definition.',
+    'Retrieve a copy of the requested worker type definition.',
     'This copy contains a lastModified field as well as the worker',
     'type name.  As such, it will require manipulation to be able to',
     'use the results of this method to submit date to the update',
