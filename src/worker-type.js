9157854fe008ae48fd2b9354bb0c73dbc2774b5e
Dustin J. Mitchell <dustin@mozilla.com>
2017-09-18 21:14:11 +0000

    Bug 1400261: consider only configured AZs, when any are configured

--- src/worker-type.js
+++ src/worker-type.js
@@ -824,14 +824,31 @@ WorkerType.prototype.determineSpotBids = function(managedRegions, pricing, chang
         .filter(r => _.includes(managedRegions, r.region))
         .map(r => r.region);
 
+    let configuredAzs = {};
+    this.availabilityZones.forEach(az => {
+      if (az.region in configuredAzs) {
+        configuredAzs[az.region].push(az.availabilityZone);
+      } else {
+        configuredAzs[az.region] = [az.availabilityZone];
+      }
+    });
+
     let priceTrace = [];
 
     for (let region of regions) {
       for (let type of types) {
         let zones = [];
+
         if (pricingData[region] && pricingData[region][type]) {
           zones = Object.keys(pricingData[region][type]);
         }
+
+        // if we have AZ configuration, consider only the configured AZs
+        if (region in configuredAzs) {
+          const configured = configuredAzs[region];
+          zones = zones.filter(z => configured.includes(z));
+        }
+
         for (let zone of zones) {
           try {
             let potentialBid = pricingData[region][type][zone];
